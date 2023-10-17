7039417402a1d6476c03bd59c32a26e49c3a3a03
amretasrerengarajan <rengarajanthiruven.a@northeastern.edu>
2023-10-17 10:12:45 -0400

    Added file path of packer

--- validate-packer.sh
+++ validate-packer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Run 'packer init' to ensure required plugins are installed
-packer init
+# packer init
 
-if ! packer validate packer/; then
+if ! packer validate packer/aws-ami.pkr.hcl; then
   echo "Packer file contains configuration errors. Please fix the issues."
   exit 1
 else
