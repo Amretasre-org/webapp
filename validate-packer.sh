07505126c4f4097f19f9893136c6fb940daba051
amretasrerengarajan <rengarajanthiruven.a@northeastern.edu>
2023-10-17 10:06:22 -0400

    Added packer init

--- validate-packer.sh
+++ validate-packer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Run 'packer init' to ensure required plugins are installed
+packer init
+
 if ! packer validate packer/; then
   echo "Packer file contains configuration errors. Please fix the issues."
   exit 1
