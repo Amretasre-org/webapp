e337dc0a6a8b6bd786b50251feacd2e35b7cdd7f
amretasrerengarajan <rengarajanthiruven.a@northeastern.edu>
2023-10-18 17:58:35 -0400

    Change gitaction for build

--- debian-build-node-app.sh
+++ debian-build-node-app.sh
@@ -22,24 +22,34 @@ cd /opt/ || exit
 # Print the directory structure
 ls -R /opt/
 
-sudo unzip webapp.zip
+zip_file="/opt/webapp.zip"
+
+# Check if the ZIP file exists
+if [ -f "$zip_file" ]; then
+  # Unzip the file to the specified destination folder
+  unzip "$zip_file" 
+else
+  echo "ZIP file not found: $zip_file"
+fi
+
+# sudo unzip webapp.zip
 
 # Change directory to the unzipped webapp directory, exit if cd fails
-if cd webapp; then
+# if cd webapp; then
+
+#   echo "HOST=127.0.0.1" > .env
+# 	echo "MYSQLUSER=root" >> .env
+#   echo "PASSWORD=soumya6225" >> .env
   
-  echo "HOST=127.0.0.1" > .env
-	echo "MYSQLUSER=root" >> .env
-  echo "PASSWORD=soumya6225" >> .env
-  
-  # Run npm install
-  echo "cd inside the webapp directory"
-  if sudo npm i; then
-    echo "npm install completed without errors."
-  else
-    echo "npm install encountered errors."
-    exit 1
-  fi
-else
-  echo "Failed to change directory to /webapp. Exiting."
-  exit 1
-fi
+#   # Run npm install
+#   echo "cd inside the webapp directory"
+#   if sudo npm i; then
+#     echo "npm install completed without errors."
+#   else
+#     echo "npm install encountered errors."
+#     exit 1
+#   fi
+# else
+#   echo "Failed to change directory to /webapp. Exiting."
+#   exit 1
+# fi
