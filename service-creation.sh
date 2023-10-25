12d7d7e328fb44d412de44dc0d17376b0010c2ef
amretasrerengarajan <rengarajanthiruven.a@northeastern.edu>
2023-10-25 17:57:40 -0400

    Added permissions and source to env

--- service-creation.sh
+++ service-creation.sh
@@ -20,7 +20,7 @@ cat <<EOF | sudo tee /etc/systemd/system/$APP_NAME.service
 [Unit]
 Description=$APP_NAME
 ConditionPathExists=/opt/dist/
-After=cloud-init.service
+After=network.target
 
 [Service]
 Type=simple
@@ -43,5 +43,12 @@ EOF
 
 # Set permissions on the service unit file
 sudo chmod 664 "/etc/systemd/system/$APP_NAME.service"
-sudo chown $USER:csye6225 /var/log/$APP_NAME.service/
+sudo chown $USER:$GROUP /var/log/$APP_NAME.service/
 sudo chmod 755 /var/log/$APP_NAME.service/
+
+sudo chown -R $USER:$GROUP /opt/dist
+sudo chmod -R 755 /opt/dist
+# sudo chown -R $USER:$GROUP /etc/environment
+# sudo chmod 644 /etc/environment
+
+sudo systemctl daemon-reload
--- start-app.sh
+++ start-app.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+USER="systemd-user"
+GROUP="csye6225"
+
+sudo chown -R $USER:$GROUP /etc/environment
+sudo chmod 644 /etc/environment
+
+# Source the environment variables from /etc/environment
+source /etc/environment
+
 # Change to the directory where your Node.js application is located
 cd /opt/dist
 
-echo "HOST=$HOST" > .env
-echo "MYSQLUSER=$MYSQLUSER" >> .env
-echo "PASSWORD=$PASSWORD" >> .env
+# "HOST=$HOST" > .env
+# "MYSQLUSER=$MYSQLUSER" >> .env
+# "PASSWORD=$PASSWORD" >> .env
 
 # Start your Node.js application using npm
 npm run prd-start
