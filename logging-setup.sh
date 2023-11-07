cd00d53145be582c4caa7e36e58fdbbe297a1eb4
amretasrerengarajan <rengarajanthiruven.a@northeastern.edu>
2023-11-07 00:57:58 -0500

    Changes made for statsd

--- logging-setup.sh
+++ logging-setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# cloud watch installation
+# Install and configure the CloudWatch Agent using wget
+sudo wget https://amazoncloudwatch-agent-us-east-1.s3.us-east-1.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a append-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
-
